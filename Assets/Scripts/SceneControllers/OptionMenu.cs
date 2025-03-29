f686270da24a791dff668b68a3ad8e483b575a31
FebaDev <felipebposada@gmail.com>
2025-03-29 00:34:08 -0500

    UI Created in Gameplay Scene

--- Assets/Scripts/SceneControllers/OptionMenu.cs
+++ Assets/Scripts/SceneControllers/OptionMenu.cs
@@ -1,97 +1,202 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Linq;
+using System.Collections.Generic;
+
 public class OptionMenu : MonoBehaviour
 {
+    [Header("Audio Settings")]
     public Slider fxSlider;
     public Slider musicSlider;
     public Slider generalSlider;
+    [SerializeField] private Toggle muteToggle;
 
-    [SerializeField] private Toggle muteToggle;         // Toggle para silenciar/desilenciar
+    [Header("Display Settings")]
+    public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+
     private float previousVolume;
+    private Resolution[] resolutions;
+    private bool isInitializing = true;
+
     void Awake()
     {
-        float sfxVolume = PlayerPrefs.GetFloat("FxVolume", 0f); // Valor predeterminado en 0 dB
-        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", 0f); // Valor predeterminado en 0 dB
-        float generalVolume = PlayerPrefs.GetFloat("GeneralVolume", 0f); // Valor predeterminado en 0 dB   
+        // Cargar configuraciones de audio con valores por defecto (75%)
+        float sfxVolume = PlayerPrefs.GetFloat("FxVolume", 0.75f);
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        float generalVolume = PlayerPrefs.GetFloat("GeneralVolume", 0.75f);
+
         fxSlider.value = sfxVolume;
         musicSlider.value = musicVolume;
         generalSlider.value = generalVolume;
 
-        SetFxVolume(sfxVolume);
-        SetGeneralVolume(musicVolume);
-        SetMusicVolume(musicVolume);
+        // Aplicar valores iniciales
+        SetFxVolume(sfxVolume, true);
+        SetMusicVolume(musicVolume, true);
+        SetGeneralVolume(generalVolume, true);
+
+        // Configurar pantalla
+        SetupResolutionDropdown();
+        LoadDisplaySettings();
     }
+
     void Start()
     {
-        // Asignar los métodos a los eventos de cambio de valor de los sliders
-        fxSlider.onValueChanged.AddListener(SetFxVolume); 
-        musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        generalSlider.onValueChanged.AddListener(SetGeneralVolume);
+        isInitializing = false;
 
+        // Configurar listeners de audio
+        fxSlider.onValueChanged.AddListener((volume) => SetFxVolume(volume));
+        musicSlider.onValueChanged.AddListener((volume) => SetMusicVolume(volume));
+        generalSlider.onValueChanged.AddListener((volume) => SetGeneralVolume(volume));
         muteToggle.onValueChanged.AddListener(OnMuteToggleChanged);
+
         InitializeToggleState();
+
+        // Configurar listeners de pantalla
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+        fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
     }
 
+    #region Audio Settings
     void InitializeToggleState()
     {
-        //muteToggle.isOn = false;
         float currentVolume;
-        AudioManager.Instance.audioMixer.GetFloat("Master",out currentVolume);
-        muteToggle.isOn = currentVolume == -80f;
+        AudioManager.Instance.audioMixer.GetFloat("Master", out currentVolume);
+        muteToggle.isOn = currentVolume <= -80f;
     }
+
     void OnMuteToggleChanged(bool isMuted)
     {
         if (isMuted)
         {
-            // Guarda el volumen actual antes de mutear
             AudioManager.Instance.audioMixer.GetFloat("Master", out previousVolume);
-
-            // Silencia el volumen (establece el volumen a -80 dB)
             AudioManager.Instance.audioMixer.SetFloat("Master", -80f);
         }
         else
         {
-            // Restaura el volumen anterior
             AudioManager.Instance.audioMixer.SetFloat("Master", previousVolume);
         }
+        PlayerPrefs.SetInt("IsMuted", isMuted ? 1 : 0);
+        PlayerPrefs.Save();
     }
+
     public void SetFxVolume(float volume)
     {
-        Debug.Log($"Setting FX Volume: {volume}");
-        if (AudioManager.Instance.audioMixer != null)
+        SetFxVolume(volume, false);
+    }
+
+    private void SetFxVolume(float volume, bool initialLoad)
+    {
+        if (AudioManager.Instance?.audioMixer != null)
         {
-            // Asigna directamente el valor en dB al AudioMixer
-            AudioManager.Instance.audioMixer.SetFloat("Fx", volume);
-            PlayerPrefs.SetFloat("FxVolume", volume); // Guarda el valor
+            float dB = volume > 0 ? 20f * Mathf.Log10(volume) : -80f;
+            AudioManager.Instance.audioMixer.SetFloat("Fx", dB);
+
+            if (!initialLoad && !isInitializing)
+            {
+                PlayerPrefs.SetFloat("FxVolume", volume);
+                PlayerPrefs.Save();
+            }
         }
     }
 
-    // Método para ajustar el volumen de la música
     public void SetMusicVolume(float volume)
     {
-        Debug.Log($"Setting Music Volume: {volume}");
-        if (AudioManager.Instance.audioMixer != null)
+        SetMusicVolume(volume, false);
+    }
+
+    private void SetMusicVolume(float volume, bool initialLoad)
+    {
+        if (AudioManager.Instance?.audioMixer != null)
         {
-            // Asigna directamente el valor en dB al AudioMixer
-            AudioManager.Instance.audioMixer.SetFloat("Music", volume);
-            PlayerPrefs.SetFloat("MusicVolume", volume); // Guarda el valor
+            float dB = volume > 0 ? 20f * Mathf.Log10(volume) : -80f;
+            AudioManager.Instance.audioMixer.SetFloat("Music", dB);
+
+            if (!initialLoad && !isInitializing)
+            {
+                PlayerPrefs.SetFloat("MusicVolume", volume);
+                PlayerPrefs.Save();
+            }
         }
     }
-    // Método para ajustar el volumen del general
+
     public void SetGeneralVolume(float volume)
     {
-        Debug.Log($"Setting General Volume: {volume}");
-        if (AudioManager.Instance.audioMixer != null)
+        SetGeneralVolume(volume, false);
+    }
+
+    private void SetGeneralVolume(float volume, bool initialLoad)
+    {
+        if (AudioManager.Instance?.audioMixer != null)
         {
-            // Asigna directamente el valor en dB al AudioMixer
-            AudioManager.Instance.audioMixer.SetFloat("Master", volume);
-            PlayerPrefs.SetFloat("GeneralVolume", volume); // Guarda el valor
+            float dB = volume > 0 ? 20f * Mathf.Log10(volume) : -80f;
+            AudioManager.Instance.audioMixer.SetFloat("Master", dB);
+
+            if (!initialLoad && !isInitializing)
+            {
+                PlayerPrefs.SetFloat("GeneralVolume", volume);
+                PlayerPrefs.Save();
+            }
         }
     }
+    #endregion
+
+    #region Display Settings
+    void SetupResolutionDropdown()
+    {
+        resolutions = Screen.resolutions
+            .Where(res => res.refreshRateRatio.Equals(Screen.currentResolution.refreshRateRatio))
+            .Distinct()
+            .ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = $"{resolutions[i].width} x {resolutions[i].height}";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    void LoadDisplaySettings()
+    {
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", Screen.fullScreen ? 1 : 0) == 1;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
+        PlayerPrefs.Save();
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+    #endregion
+
     public void clicSound()
     {
-        AudioManager.Instance.PlayFX("start");
+        AudioManager.Instance?.PlayFX("start");
     }
 
     public void exitMenu()
@@ -99,6 +204,4 @@ public class OptionMenu : MonoBehaviour
         PlayerPrefs.Save();
         SceneManager.UnloadSceneAsync("OptionMenu");
     }
-
-
 }
