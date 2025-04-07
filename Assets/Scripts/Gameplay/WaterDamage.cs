de3943a8ad99dc8568ec38f453414ce5f5740718
Kaitokid724 <d.valencia710@gmail.com>
2025-04-07 11:51:54 -0500

    Colliders y Daño agua

---
+++ Assets/Scripts/Gameplay/WaterDamage.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class WaterDamage : MonoBehaviour
+{
+    public float damagePerSecond = 5f;
+    private bool isPlayerInWater = false;
+    private HealthBar playerHealth;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHealth = other.GetComponent<HealthBar>();
+
+            if (playerHealth == null)
+            {
+                Debug.LogError("No se encontró el componente HealthBar en el jugador.");
+            }
+            else
+            {
+                isPlayerInWater = true;
+                Debug.Log("Jugador entró al agua, comenzará a recibir daño.");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInWater = false;
+            Debug.Log("Jugador salió del agua, ya no recibe daño.");
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerInWater && playerHealth != null)
+        {
+            playerHealth.TakeDamage(damagePerSecond * Time.deltaTime);
+        }
+    }
+}
